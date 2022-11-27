b0a75f32ec00e286a4e98cc31847b7f7566afbe1
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-27 17:05:45 +0100

    Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'web-store',
-    password: 'password'
+const sequelize = new Sequelize('web-store', 'root', 'password', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
