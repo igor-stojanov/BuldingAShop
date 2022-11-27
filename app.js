ca9c7c343a0796d2efc21c67f193ecd35d896abd
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-27 15:45:08 +0100

    MySQL

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
-
+const db = require('./util/database');
 
 const app = express();
 
@@ -13,6 +13,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
