ee39446b6492f2f7f0ef926b7aab78ce15724983
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-27 17:38:54 +0100

    Sequelize

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -22,4 +22,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result => {
+    //console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+});
+
+
