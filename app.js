32c2ca258d1bcf4ca18ddc0a75d683185db0671b
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-28 19:06:30 +0100

    Cart

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -33,14 +35,19 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// relations:
 Product.belongsTo(User, {
     constraints: true, onDelete: 'CASCADE'
 });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-// .sync({force: true})
-.sync()
+.sync({force: true})
+//.sync()
 .then(result => {
     return User.findByPk(1);
     //console.log(result);
