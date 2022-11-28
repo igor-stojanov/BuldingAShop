db23d81ab3fd7ade399486373758d33d1841b544
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-28 19:55:27 +0100

    Cart - update

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-.sync({force: true})
-//.sync()
+//.sync({force: true})
+.sync()
 .then(result => {
     return User.findByPk(1);
     //console.log(result);
@@ -60,7 +60,10 @@ sequelize
     return user;
 })
 .then(user => {
-    console.log(user);
+    return user.createCart();
+    
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
