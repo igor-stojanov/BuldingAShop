1da1274829e2386e792203c58f403eb4fea577b2
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-29 19:26:02 +0100

    Cart - Orders

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
-
-// pug
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,14 +20,14 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
+  User.findByPk(1)
     .then(user => {
-        req.user = user;
-        next();
+      req.user = user;
+      next();
     })
     .catch(err => console.log(err));
 });
@@ -35,39 +37,36 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// relations:
-Product.belongsTo(User, {
-    constraints: true, onDelete: 'CASCADE'
-});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-//.sync({force: true})
-.sync()
-.then(result => {
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
     return User.findByPk(1);
-    //console.log(result);
-    
-})
-.then(user => {
-    if(!user) {
-        User.create({name: 'Igor', email: 'igorstojanov8@gmail.com'})
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
     return user;
-})
-.then(user => {
+  })
+  .then(user => {
+    // console.log(user);
     return user.createCart();
-    
-})
-.then(cart => {
+  })
+  .then(cart => {
     app.listen(3000);
-})
-.catch(err => {
+  })
+  .catch(err => {
     console.log(err);
-});
-
-
+  });
