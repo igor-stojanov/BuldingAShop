3bb4f3012e39209f9f6f808853d03a4dcbe0f21d
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-29 20:59:34 +0100

    MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+// --- MySQL ---
 
-const sequelize = new Sequelize('web-store', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const Sequelize = require('sequelize');
 
-module.exports = sequelize;
+// const sequelize = new Sequelize('web-store', 'root', 'password', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+
+// module.exports = sequelize;
+
+// --- MySQL ---
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = callabck => {
+  MongoClient.connect('mongodb+srv://igorstojanov:ballantines1827@cluster.fwpzewt.mongodb.net/?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected!');
+    callabck(client);
+  })
+  .catch(err => console.log(err)); // connecting to the mongoDB
+}
+
+module.exports = mongoConnect;
