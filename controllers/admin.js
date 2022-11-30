9fded1621dcf3561b842e436afb2a1bf31f694c0
Igor Stojanov <stojanovigor99@hotmail.com>
2022-11-30 18:32:57 +0100

    MongoDB

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,21 +13,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
-    .then(result => {
+  const product = new Product(title, price, description, imageUrl);
+  // req.user MySQL
+  //   .createProduct({
+  //     title: title,
+  //     price: price,
+  //     imageUrl: imageUrl,
+  //     description: description
+  //   }) /MySQL
+  product
+  .save()
+  .then(result => {
       // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
-    .catch(err => {
-      console.log(err);
-    });
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 // --- MySQL ---
